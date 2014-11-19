31b949b2b0d5dfd36d9cd5b8fad92204edae3187
TOKENHACKATON <dalphin.j@laposte.net>
2014-11-19 11:33:18 +0100

    update

--- www/js/main.js
+++ www/js/main.js
@@ -1,15 +1,38 @@
 
 
-document.addEventListener("deviceready", onDeviceReady, false);
+function sucessGeo(position, f, op){
+    console.log('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
 
+}
+
+function failGeo(e){
+    // erreur de GeoLoc
+    console.log(e);
+}
+
+function startFunction(){
+    alert("startFunction")
+}
+
+
+/////////////////////////////////////////////////////////////
 function onDeviceReady() {
+    console.log("deviceready");
     try {
         if (navigator.geolocation !== null) {
-            var options = { timeout: 100, maximumAge: 11000, enableHighAccuracy: true};
+            console.log("navigator.geolocation !== null");
+            var options = { timeout: 100, maximumAge: 11000, enableHighAccuracy: true };
             navigator.geolocation.watchPosition(sucessGeo, failGeo, options);
         }
     } catch (e) {
-        //alert(e.message); // une erreur est survenu
+        console(e.message); // une erreur est survenu
     }
 
     try {
@@ -18,19 +41,4 @@ function onDeviceReady() {
     } catch (e) {}
 }
 
-function sucessGeo(position){
-    //console.log('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-}
-
-function failGeo(){
-    // erreur de GeoLoc
-}
-
-
+document.addEventListener("deviceready", onDeviceReady, false);
