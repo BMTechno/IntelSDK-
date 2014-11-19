3a8f61c6e2fe2beae771f9ccac765b10b9599bb5
updatepixels <updatepixels@gmail.com>
2014-11-19 11:08:03 +0100

    Cleaning Code

--- www/js/main.js
+++ www/js/main.js
@@ -1,107 +1,15 @@
-﻿/*jslint sloppy:true, browser:true, devel:true, white:true, vars:true, eqeq:true, nomen:true, unparam:true */
-/*global intel, google, Marker, device */
-
-var _map = null;
-var _seconds = 30;
-var _llbounds = null;
-var myLatLng;
-var oldLatLng = "";
-var boolTripTrack = true;
-//Create the google Maps and LatLng object 
-
-function drawMap() {
-    //Creates a new google maps object
-    var latlng = new google.maps.LatLng(currentLatitude, currentLongitude);
-    myLatLng = latlng;
-    var mapOptions = {
-        center: latlng,
-        zoom:7,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.LEFT_TOP
-        }
-    };
-    if (boolTripTrack === true) {
-        _map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    }
-}
-//40.7655,-73.97204 = NYC
-var currentLatitude = "40.713768";
-var currentLongitude = "-73.016696";
-var options = {
-    timeout: 10000,
-    maximumAge: 11000,
-    enableHighAccuracy: true
-};
-//Success callback
-var suc = function(p) {
-        console.log("geolocation success", 4);
-        //Draws the map initially
-        if (_map === null) {
-            currentLatitude = p.coords.latitude;
-            currentLongitude = p.coords.longitude;
-            drawMap();
-        } else {
-            myLatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
-        }
-        //Creates a new google maps marker object for using with the pins
-        if ((myLatLng.toString().localeCompare(oldLatLng.toString())) !== 0) {
-            //Create a new map marker
-            var Marker = new google.maps.Marker({
-                position: myLatLng,
-                map: _map
-            });
-            if (_llbounds === null) {
-                //Create the rectangle in geographical coordinates
-                _llbounds = new google.maps.LatLngBounds(new google.maps.LatLng(p.coords.latitude, p.coords.longitude)); //original
-            } else {
-                //Extends geographical coordinates rectangle to cover current position
-                _llbounds.extend(myLatLng);
-            }
-            //Sets the viewport to contain the given bounds & triggers the "zoom_changed" event
-            _map.fitBounds(_llbounds);
-        }
-        oldLatLng = myLatLng;
-    };
-var fail = function() {
-        console.log("Geolocation failed. \nPlease enable GPS in Settings.", 1);
-    };
-var getLocation = function() {
-        console.log("in getLocation", 4);
-    };
-    //Execute when the DOM loads  
 
 
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     try {
-        if (device.platform.indexOf("Android") != -1) {
-            intel.xdk.display.useViewport(480, 480);
-            document.getElementById("map_canvas").style.width = "480px";
-        } 
-        else if (device.platform.indexOf("iOS") != -1) {
-            if (device.model.indexOf("iPhone") != -1 || device.model.indexOf("iPod") != -1) {
-                intel.xdk.display.useViewport(320, 320);
-                document.getElementById("map_canvas").style.width = "320px";
-            } else if (device.model.indexOf("iPad") != -1) {
-                intel.xdk.display.useViewport(768, 768);
-                document.getElementById("map_canvas").style.width = "768px";
-            }
-        }
-        if (device.platform.indexOf("Win") != -1) {
-            document.getElementById("map_canvas").style.width = screen.width + "px";
-            document.getElementById("map_canvas").style.height = screen.height + "px";
-        }
-        
         if (navigator.geolocation !== null) {
-            document.getElementById("map_canvas").style.height = screen.height + "px";
-            
-            navigator.geolocation.watchPosition(logGeo, fail, { timeout: 100, maximumAge: 11000, enableHighAccuracy: true});
+            var options = { timeout: 100, maximumAge: 11000, enableHighAccuracy: true};
+            navigator.geolocation.watchPosition(sucessGeo, failGeo, options);
         }
     } catch (e) {
-        alert(e.message);
+        //alert(e.message); // une erreur est survenu
     }
 
     try {
@@ -110,8 +18,8 @@ function onDeviceReady() {
     } catch (e) {}
 }
 
-function logGeo(position){
-    //alert('Latitude: '          + position.coords.latitude          + '\n' +
+function sucessGeo(position){
+    //console.log('Latitude: '          + position.coords.latitude          + '\n' +
           'Longitude: '         + position.coords.longitude         + '\n' +
           'Altitude: '          + position.coords.altitude          + '\n' +
           'Accuracy: '          + position.coords.accuracy          + '\n' +
@@ -121,5 +29,8 @@ function logGeo(position){
           'Timestamp: '         + position.timestamp                + '\n');
 }
 
+function failGeo(){
+    // erreur de GeoLoc
+}
 
-document.addEventListener("deviceready", onDeviceReady, false);
+
